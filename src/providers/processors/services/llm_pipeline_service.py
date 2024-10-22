491723fe5da4578e15f60d823322b2010d49f8ff
itay.dar <itay.dar@lemonade.com>
2024-10-22 03:25:07 +0300

    add audit messages into mongodb

--- src/providers/processors/services/llm_pipeline_service.py
+++ src/providers/processors/services/llm_pipeline_service.py
@@ -7,49 +7,30 @@ from tqdm import tqdm
 
 from src.providers.processors.services.ner_service import NERService
 from src.providers.processors.services.translation_service import TranslationService
+from src.providers.telegram.telegram_document import TelegramMessage
+from src.providers.logger.logger_service import Logger
 
 
 @Injectable()
 class LLMPipelineService:
     ABS_PATH = Path(__file__).resolve().parent.parent.parent.parent
 
-    def __init__(self, translator: TranslationService, ner_service: NERService):
+    def __init__(self, translator: TranslationService, ner_service: NERService, logger: Logger):
         self.translator = translator
         self.ner_service = ner_service
+        self.logger = logger
 
-    def process_dataframe(self, df: pd.DataFrame) -> pd.DataFrame:
-        df = df.copy()
-        df["hebrew_translation"] = None
-        df["english_translation"] = None
-        df["locations"] = None
-        df["people"] = None
-        df["organizations"] = None
-
-        for index, row in tqdm(
-            df.iterrows(), total=len(df), desc="Processing messages"
-        ):
+    async def process_messages(self, messages: list[TelegramMessage]) -> list[TelegramMessage]:
+        for msg in messages:
             try:
-                message = row["message"]
-
-                # Translation
-                translation = self.translator.translate(message)
-                df.at[index, "hebrew_translation"] = translation.hebrew
-                # df.at[index, "english_translation"] = translation.english
-
-                # NER Extraction
-                # ner_data = self.ner_service.extract_entities(translation.english)
-                # df.at[index, "locations"] = ", ".join(ner_data.locations)
-                # df.at[index, "people"] = ", ".join(ner_data.people)
-                # df.at[index, "organizations"] = ", ".join(ner_data.organizations)
+                if msg.passed_keyword_filter and msg.passed_deduplication:
+                    # Perform translation
+                    translation = self.translator.translate(message)
+                    msg.hebrew_translation = translation.hebrew
+                    msg.english_translation = translation.english
+                    # Update the message in MongoDB
+                    await msg.save()
             except Exception as e:
-                print(e)
-                df.at[index, "hebrew_translation"] = None
+                self.logger.log.error(f"Error processing message: {e}")
 
-        self.save_dataframe(df, self.ABS_PATH / "data" / "processed_data.json")
-
-        return df
-
-    @staticmethod
-    def save_dataframe(df: pd.DataFrame, file_path: str | Path):
-        df.to_json(file_path, index=False)
-        return file_path
+        return messages
