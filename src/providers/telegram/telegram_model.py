4d1c70f3e95ed8c2b76ccc53ba2ca4c23355b570
itay.dar <itay.dar@lemonade.com>
2024-10-22 16:16:46 +0300

    take messages from 10 last minutes (not 600)

--- src/providers/telegram/telegram_model.py
+++ src/providers/telegram/telegram_model.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, BaseSettings
 class TelegramSettings(BaseSettings):
     SESSION_NAME: str = "session_name"
     FETCH_LIMIT: int = 1000
-    MESSAGE_FILTER_INTERVAL_MINUTES: int = 10 * 60
+    MESSAGE_FILTER_INTERVAL_MINUTES: int = 10
 
 
 CHANNELS = [
