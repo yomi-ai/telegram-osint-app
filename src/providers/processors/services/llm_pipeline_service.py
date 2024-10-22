fcabbad3987987a253ebabb5d0e180d1b131a7e7
itay.dar <itay.dar@lemonade.com>
2024-10-22 10:38:09 +0300

    run black

--- src/providers/processors/services/llm_pipeline_service.py
+++ src/providers/processors/services/llm_pipeline_service.py
@@ -15,23 +15,31 @@ from src.providers.logger.logger_service import Logger
 class LLMPipelineService:
     ABS_PATH = Path(__file__).resolve().parent.parent.parent.parent
 
-    def __init__(self, translator: TranslationService, ner_service: NERService, logger: Logger):
+    def __init__(
+        self, translator: TranslationService, ner_service: NERService, logger: Logger
+    ):
         self.translator = translator
         self.ner_service = ner_service
         self.logger = logger
 
-    async def process_messages(self, messages: list[TelegramMessage]) -> list[TelegramMessage]:
+    async def process_messages(
+        self, messages: list[TelegramMessage]
+    ) -> list[TelegramMessage]:
         for msg in messages:
             try:
                 if msg.passed_keyword_filter and msg.passed_deduplication is True:
                     # Perform translation
-                    self.logger.info(f"Translating message - {msg.channel}:{msg.message_id}")
+                    self.logger.info(
+                        f"Translating message - {msg.channel}:{msg.message_id}"
+                    )
                     translation = self.translator.translate(msg.content)
                     msg.hebrew_translation = translation.hebrew
                     msg.english_translation = translation.english
                     # Update the message in MongoDB
                     await msg.save()
-                    self.logger.info(f"Finish translating message {msg.channel}:{msg.message_id}")
+                    self.logger.info(
+                        f"Finish translating message {msg.channel}:{msg.message_id}"
+                    )
             except Exception as e:
                 self.logger.log.error(f"Error processing message: {e}")
                 continue
